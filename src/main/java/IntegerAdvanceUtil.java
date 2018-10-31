e00a27d0a27d1a04a25a27e326a1b6edf64e4a09
dunso <dunso@dun.so>
2018-10-31 23:19:11 +0800

    Add IntegerAdvanceUtils

---
+++ src/main/java/IntegerAdvanceUtil.java
@@ -0,0 +1,18 @@
+/**
+ * Integer Advanced Tool
+ *
+ * @Author dunso
+ */
+public class IntegerAdvanceUtil {
+
+  /**
+   * Judge an int is pow of 2
+   *
+   * @return if val is pow of 2 return true, else false
+   */
+  public static boolean isPowOf2(int val) {
+    return (val & -val) == val;
+  }
+
+
+}
